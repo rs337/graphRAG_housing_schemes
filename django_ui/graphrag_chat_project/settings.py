b8c4430d9fe2613444513dc6950c1ffedeb8b2fd
sparksr2 <robert.sparks2@mail.dcu.ie>
2025-07-31 21:26:53 +0100

    ip address settings

--- django_ui/graphrag_chat_project/settings.py
+++ django_ui/graphrag_chat_project/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', "django-insecure-jd2q#)(wyc1f1ox15sl*ji*_3a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',') if os.getenv('ALLOWED_HOSTS') else ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',') if os.getenv('ALLOWED_HOSTS') else ['localhost', '127.0.0.1', '13.60.5.136']
 
 
 # Application definition
